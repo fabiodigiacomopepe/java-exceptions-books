bab359345cef50728aa45f5bb0c6c7177a3ccc88
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-10-24 15:31:09 +0200

    Basic request: DONE

--- src/org/lessons/java/exceptions/Libro.java
+++ src/org/lessons/java/exceptions/Libro.java
@@ -1,2 +1,86 @@
-package org.lessons.java.exceptions;public class Libro {
+package org.lessons.java.exceptions;
+
+public class Libro {
+    // ATTRIBUTI
+    private String titolo;
+    private int numero_pagine;
+    private String autore;
+    private String editore;
+
+    // COSTRUTTORI
+    public Libro(String titolo, int numero_pagine, String autore, String editore) throws Exception {
+        // Verifico che numero di pagine non sia <=0
+        if (titolo.isEmpty()) {
+            throw new Exception("Il titolo è obbligatorio.");
+        } else if (numero_pagine <= 0) {
+            throw new Exception("Il numero delle pagine deve essere maggiore di 0.");
+        } else if (autore.isEmpty()) {
+            throw new Exception("L'autore è obbligatorio.");
+        } else if (editore.isEmpty()) {
+            throw new Exception("L'editore è obbligatorio.");
+        }
+
+        this.titolo = titolo;
+        this.numero_pagine = numero_pagine;
+        this.autore = autore;
+        this.editore = editore;
+    }
+
+    //METODI
+
+    // TITOLO
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        if (titolo.isEmpty()) {
+            throw new IllegalArgumentException("Il titolo è obbligatorio.");
+        }
+        this.titolo = titolo;
+    }
+
+    // NUMERO PAGINE
+    public int getNumero_pagine() {
+        return numero_pagine;
+    }
+
+    public void setNumero_pagine(int numero_pagine) {
+        if (numero_pagine <= 0) {
+            throw new IllegalArgumentException("Il numero delle pagine deve essere maggiore di 0.");
+        }
+        this.numero_pagine = numero_pagine;
+    }
+
+    // AUTORE
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        if (autore.isEmpty()) {
+            throw new IllegalArgumentException("L'autore è obbligatorio.");
+        }
+        this.autore = autore;
+    }
+
+    // EDITORE
+    public String getEditore() {
+        return editore;
+    }
+
+    public void setEditore(String editore) {
+        if (editore.isEmpty()) {
+            throw new IllegalArgumentException("L'editore è obbligatorio.");
+        }
+        this.editore = editore;
+    }
+
+    @Override
+    public String toString() {
+        return "Titolo: " + titolo +
+                "\nNumero pagine: " + numero_pagine +
+                "\nAutore: " + autore +
+                "\nEditore: " + editore;
+    }
 }
--- src/org/lessons/java/exceptions/Main.java
+++ src/org/lessons/java/exceptions/Main.java
@@ -1,2 +1,101 @@
-package org.lessons.java.exceptions;public class Main {
+package org.lessons.java.exceptions;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Inizializzo lo scanner da tastiera
+        Scanner scan = new Scanner(System.in);
+
+        // Chiedo a utente quanti libri vuole aggiungere
+        int bookNumber = 0;
+        boolean exitNumberBook = false;
+        while (!exitNumberBook) {
+            // Chiedo numero pagine libro a utente
+            System.out.println("Quanti libri vuoi aggiungere?");
+            System.out.print("Inserisci un numero: ");
+            String bookNumberString = scan.nextLine();
+            // Provo a convertire il numero ricevuto dallo scanner in un intero
+            try {
+                bookNumber = Integer.parseInt(bookNumberString);
+                // Se riesco a convertire ed è maggiore di 0
+                if (bookNumber > 0) {
+                    exitNumberBook = true;
+                } // Altrimenti
+                else {
+                    System.out.println("Inserire un numero maggiore di 0.");
+                    exitNumberBook = false;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Inserire un numero.");
+                exitNumberBook = false;
+            }
+        }
+
+        // Creo array di libri con numero preso da input utente tramite scanner
+        Libro[] libri = new Libro[bookNumber];
+
+        for (int i = 0; i < libri.length; i++) {
+            // Inizializzo bookNumeroPagineString e bookNumeroPagine
+            // Per usarli sia nel try che nel catch
+            String bookNumeroPagineString = "";
+            int bookNumeroPagine = 0;
+
+            // Inizializzo condizione per uscire dal ciclo while uguale a false
+            boolean exit = false;
+
+            // Creo loop finchè exit non diventa true
+            while (!exit) {
+                try {
+                    // Chiedo titolo libro a utente
+                    System.out.print("Inserisci il titolo: ");
+                    String bookTitolo = scan.nextLine();
+
+                    boolean exitPagine = false;
+                    while (!exitPagine) {
+                        // Chiedo numero pagine libro a utente
+                        System.out.print("Inserisci il numero delle pagine: ");
+                        bookNumeroPagineString = scan.nextLine();
+                        try {
+                            bookNumeroPagine = Integer.parseInt(bookNumeroPagineString);
+                            exitPagine = true;
+                        } catch (NumberFormatException e) {
+                            System.out.println("Inserire un numero.");
+                            exitPagine = false;
+                        }
+                    }
+
+                    // Chiedo autore libro a utente
+                    System.out.print("Inserisci l'autore: ");
+                    String bookAutore = scan.nextLine();
+
+                    // Chiedo editore libro a utente
+                    System.out.print("Inserisci l'editore: ");
+                    String bookEditore = scan.nextLine();
+
+                    // Creo un nuovo oggetto libro
+                    Libro libro = new Libro(bookTitolo, bookNumeroPagine, bookAutore, bookEditore);
+                    libri[i] = libro;
+
+                    // Setto exit a true per uscire dal loop
+                    exit = true;
+                } catch (Exception e) {
+                    // Stampo il messaggio di errore;
+                    System.out.println(e.getMessage());
+                    exit = false;
+                }
+            }
+
+            System.out.println("-------------");
+        }
+
+        // Chiudo lo scanner da tastiera
+        scan.close();
+
+        //Stampo info ogni libro
+        for (Libro singolo_libro : libri) {
+            System.out.println(singolo_libro);
+            System.out.println("-------------");
+        }
+    }
 }
